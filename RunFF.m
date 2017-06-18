afcd9225f9f90711f7c5ea634db76c7fb7d79fb9
dhingratul <atul.dhingra@rutgers.edu>
2017-06-18 19:56:54 -0400

    Documentation

--- RunFF.m
+++ RunFF.m
@@ -1,8 +1,9 @@
-p=[10,20,30,40,50]
+%% Run Fischer Faces
+p=[10,20,30,40,50];
 k=100;
 ctr=1;
 for idx=p
     [train,test,labeltr,labelte]=datapartition(fea,gnd,idx);
     [~ , ~,acc(ctr,1)] = fisherfaces (train ,test ,k,labeltr,train,labelte,test ) ;
-ctr=ctr+1;
+    ctr=ctr+1;
 end
