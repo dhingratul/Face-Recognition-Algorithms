b994a646947637aa82b7a2d225faabb7a79e5a51
dhingratul <atul.dhingra@rutgers.edu>
2017-06-18 20:05:12 -0400

    Documentation

--- datapartition.m
+++ datapartition.m
@@ -1,18 +1,21 @@
+%% Partitions the data into training, testing data based on p-values(Splits)
+% Input: Features, labels, p
+% Output: Train, Test, Label-tr, Label-te
 function[train,test,labeltr,labelte]=datapartition(features,labels,p)
 train=[];test=[];labelte=[];labeltr=[];
-%% Input features, labels, per(centage) of data you want to see, and 
-% split betwen tr and te data; by default it is set to 0.1 
+%% Input features, labels, per(centage) of data you want to see, and
+% split betwen tr and te data; by default it is set to 0.1
 if nargin<3
     p=10;
 end
-C=length(unique(labels));    
+C=length(unique(labels));
 for j=1:C
     idx=find(labels==j);
     startidx=idx(1,1);endidx=idx(end,1);
     test=[test;features(startidx:startidx+p-1,:)];
     labelte=[labelte;labels(startidx:startidx+p-1,1)];
     train=[train;features(startidx+p:endidx,:)];
-    labeltr=[labeltr;labels(startidx+p:endidx,1)];    
+    labeltr=[labeltr;labels(startidx+p:endidx,1)];
     
 end
 end
