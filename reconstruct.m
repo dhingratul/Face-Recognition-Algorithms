2c002d3732a5e5ab57421a3a119da9efa0fcde9d
dhingratul <atul.dhingra@rutgers.edu>
2017-06-18 20:07:18 -0400

    Documentation

--- reconstruct.m
+++ reconstruct.m
@@ -1,7 +1,10 @@
+%% Reconstruction error
+% Input: W, Y, mu
+% Output: X
 function X = reconstruct (W , Y , mu )
 if( nargin <3)
-X = Y * W';
+    X = Y * W';
 else
-X = Y * W' + repmat (mu , size (Y,1 ) , 1) ;
+    X = Y * W' + repmat (mu , size (Y,1 ) , 1) ;
 end
 end
