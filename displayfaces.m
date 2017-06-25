d2179f8fa60805b2b09bd48816e7ed4558811a8f
dhingratul <atul.dhingra@rutgers.edu>
2017-06-25 13:46:31 -0400

    Added Documentation

--- displayfaces.m
+++ displayfaces.m
@@ -1,9 +1,12 @@
+%% Displays the first k eigenfaces
+% Input: eigennfaces, k
+% Output: image
 function[]=displayfaces(eigenfaces,k)
-    % display the eigenvectors
-    figure;
-    for n = 1:k
+% display the eigenvectors
+figure;
+for n = 1:k
     subplot(2, ceil(k/2), n);
     evector = reshape(eigenfaces(:,n), [32,32]);
     imagesc(evector);
-    end
+end
 end
